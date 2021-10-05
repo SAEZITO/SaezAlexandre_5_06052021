5bf3a2841ae5ed110f39dd1cdb7e882b886bd002
SAEZITO <hip66@hotmail.fr>
2021-10-06 01:35:39 +0200

    remaning folder

--- js/product.js
+++ js/product.js
@@ -20,6 +20,9 @@ class User {
         this.phone = phone
     }
 }
+
+       
+  
 //Fonction de récupération des données de l'api
 const request = async (url) => {
     return fetch(url)
@@ -35,13 +38,13 @@ const request = async (url) => {
 // récupération de l'id du produit dans l'url
 const idProduct = window.location.search.slice(4);
 
+
 const createProductTpl = (article) => {
     if (article?.length === 0) {
         console.error("Product page's is empty");
         return;
     }
-
-    // Recuperation pour affichage du produit
+// Recuperation pour affichage du produit
     let tpl = `
             <div class="product">
                 <div class="product__img"><img src="${article.imageUrl}">
@@ -49,12 +52,12 @@ const createProductTpl = (article) => {
                
                 <div class="product__content">
                     <div class="product__description"> 
-                        <h2 class="product__name>${article.name}</h2>
+                        <h2 class="product__name">${article.name}</h2>
 
                         <p>${article.description}</p>
 
-                         <p class="product__stock">En stock</p>
-                     </div>
+                        <p class="product__stock"> En stock </p>
+                    </div>
                     
                      <div class="product__price">
                          <p>${article.price / 100}.00€</p>
@@ -82,7 +85,7 @@ const createProductTpl = (article) => {
 
 request("http://localhost:3000/api/furniture/" + idProduct).then(article => {
     // affichage de la fiche produit       
-    document.getElementById("main").innerHTML = createProductTpl(article);
+    document.getElementById("container").innerHTML = createProductTpl(article);
 
 
     
@@ -95,54 +98,44 @@ request("http://localhost:3000/api/furniture/" + idProduct).then(article => {
                 if (quantity <= 0){
                     alert("Vous devez choisir une quantité supérrieure à 0");
                 }else{
-            //puis on ajoute le produit au localstorage
-            const product = new Product(article._id, article.name, quantity, ((article.price * quantity)/100), selectedVarnish)
-            localStorage.selectedProduct = JSON.stringify(product);
-            console.log(product);
 
-        // on récupère les données du panier, si le panier est vide on l'initialise avec un array vide
-        const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    //On les ajoutes à la classe produit puis les envois au local storage
+    const selectedProduct = new Product(article._id, article.name, quantity, ((article.price * quantity)/100), selectedVarnish);
+                localStorage.setItem("product", JSON.stringify(selectedProduct));
+ 
+    // on récupère les données du panier, si le panier est vide on l'initialise avec un array vide
+ let cart = (JSON.parse(localStorage.getItem("cart"))) || [];
 
-        // si le panier est vide, on ajoute le 1er produit
+ 
         if (cart.length == 0) {
-            cart.push(product);
-        }
+        // si le panier est vide, on ajoute le 1er produit
+            cart.push(selectedProduct);
+        }else {
         // sinon verifier si le produit selectionné existe déjà dans le localStorage pour accumuler les quantités
-        else {
-            // recherche les même id
-            const sameProducts = cart.find(product => product._id === selectedProduct._id);
+
+const sameProducts = cart.find(product => (product._id === selectedProduct._id) && (product.varnish === selectedProduct.varnish));
+            
             if (sameProducts) {
                 // quantité des produits calculée en additionnant la quantité déjà présente dans le storage et la nouvelle quantité ajoutée, idem pour prix
                 sameProducts.quantity = Number(sameProducts.quantity) + Number(selectedProduct.quantity);
                 sameProducts.price = sameProducts.price + selectedProduct.price;
             } else {
-                cart.push(product);
+                cart.push(selectedProduct);
             }
         }
+
         // ajoute l'élement dans le localStorage
-        localStorage.setItem("cart", JSON.stringify(cart));
-        
+        localStorage.setItem("cart", JSON.stringify(cart));  
+            //Ajout d'un compteur au panier pour un effet plus visuel    
+ const basketCompteur = cart.length;
+ document.getElementById("basket__compteur").textContent = basketCompteur 
+ 
     }});
+
+
+  
+                              
+
+
 });
 
-
-       // Récupération des données produit dans une instance de classe
-
-
-/*
-    const addToCart = async (event) => {
-
-                      event.preventDefault();
-                      // on affiche un message d'erreur si options non selectionnées
-                      if (selectedVarnish == "" || quantity =="") {
-                          alert("Vous devez choisir une lentille et une quantité");
-                      }else{
-
-
-
-                        // on modifie le prix, la quantité
-        selectedProduct.quantity = quantity;
-        selectedProduct.price = (selectedProduct.price/100)*quantity;
-
-      const addBasket = document.getElementById("pushProduct")
-                                .addEventListener("click", addToCart(e)); */
